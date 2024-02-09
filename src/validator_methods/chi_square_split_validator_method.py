4d856cb9806acda78da869d093d57afb4d751e17
Louie McConnell <mcconnl3@gene.com>
2024-02-09 17:24:48 -0300

    chi square split working

---
+++ src/validator_methods/chi_square_split_validator_method.py
@@ -0,0 +1,89 @@
+import itertools
+from typing import Set, Dict
+
+import numpy as np
+import pandas as pd
+import scipy.stats
+from torch.utils.data import Dataset
+
+from src.enums.enums import DataType, ValidatorMethodParameter
+from src.validator_methods.data_validator_method import DataValidatorMethod
+from src.validator_methods.chi_square_validator_method import ChiSquareValidatorMethod
+
+
+class ChiSquareSplitValidatorMethod(ChiSquareValidatorMethod):
+    @staticmethod
+    def param_keys() -> Set[ValidatorMethodParameter]:
+        """
+        Useful for documentation purposes. Lists the parameters that the validators operates on.
+
+        @return: a list of parameters for the .validate() method.
+        """
+        return {ValidatorMethodParameter.SPLIT_GROUP_SET}
+
+    @staticmethod
+    def get_method_kwargs(data_object: Dict[str, Dataset], validator_kwargs: Dict = None) -> Dict:
+        """
+        Gets the arguments for each run of the validator_method, and what to store the results under. given data_object
+        with include_filtering and the validator kwargs, as given precisely in the schema.
+
+        @param data_object: the datasets object after `include` filtering
+        @param validator_kwargs:
+        @return:
+        """
+        split_groups = data_object['split_group_set']
+        
+        kwargs_dict = {}
+
+        def get_series(column_key, dataset):
+            matrix_dict = {
+                column: [] for column in dataset.datatypes().keys()
+            }
+
+            for idx in range(dataset.__len__()):
+                sample = dataset[idx]
+                for column, column_data in sample.items():
+                    matrix_dict[column].append(column_data)
+
+            for column in dataset.datatypes().keys():
+                matrix_dict[column] = np.vstack(matrix_dict[column])
+
+            # needs to be label encoded or it breaks independence testing
+            return np.argmax(matrix_dict[column_key], axis=1)
+
+
+        for split_group_name, split_group_data_object in split_groups.items(): 
+            dataset_keys = list(split_group_data_object.keys())
+            for dataset_0_key, dataset_1_key in itertools.combinations(dataset_keys, 2):
+                dataset_0 = split_group_data_object[dataset_0_key]
+                dataset_1 = split_group_data_object[dataset_1_key]
+
+                columns_0 = sorted(list(dataset_0.datatypes().keys()))
+                columns_1 = sorted(list(dataset_1.datatypes().keys()))
+                if columns_0 != columns_1:
+                    raise Exception("Columns in datasets splits are not the same")
+                else:
+                    columns = columns_0
+
+                for column_name in columns: 
+                    assert(dataset_0.datatypes()[column_name].value == DataType.CATEGORICAL.value)
+                    kwargs_dict[f"{split_group_name}/{dataset_0_key}_vs_{dataset_1_key}/{column_name}"] = {
+                        'x': get_series(column_name, dataset_0),
+                        'y': get_series(column_name, dataset_1),
+                    }
+
+        return kwargs_dict
+
+    @staticmethod
+    def validate(x: np.array, y: np.array) -> object:
+        def create_split_array(original_data1, original_data2):
+            split_labels = np.array(['Split1', 'Split2'])  # You can customize these labels based on your needs
+            split_array1 = np.full(len(original_data1), split_labels[0])
+            split_array2 = np.full(len(original_data2), split_labels[1])
+            return np.concatenate([original_data1, original_data2]), np.concatenate([split_array1, split_array2])
+
+        data, splits = create_split_array(x, y)
+
+        df = pd.DataFrame({'data': data, 'splits': splits})
+        contingency_table = pd.crosstab(index=df['data'], columns=df['splits']).to_numpy()
+        return scipy.stats.chi2_contingency(contingency_table)
---
+++ test/validator_methods/test_chi_square_split_validator_method.py
@@ -0,0 +1,91 @@
+from typing import Dict
+
+import numpy as np
+import torch
+
+from src.datasets.synthetic_normal_dataset import SyntheticCategoricalDataset, SyntheticNormalDatasetContinuous
+from src.data_detective_engine import DataDetectiveEngine
+
+
+class TestChiSquareSplitValidatorMethod:
+    def test_positive_example(self):
+        np.random.seed(42)
+
+        test_validation_schema: Dict = {
+            "default_inclusion": False,
+            "validators": {
+                "split_covariate_data_validator": {
+                    "include": [
+                        "feature_\d+"
+                    ]
+                }
+            }
+        }
+
+        normal_dataset: SyntheticCategoricalDataset = SyntheticCategoricalDataset(p=0.5, num_cols=1, dataset_size=10000)
+        train_size: int = int(0.6 * len(normal_dataset))
+        val_size: int = int(0.2 * len(normal_dataset))
+        test_size: int = len(normal_dataset) - train_size - val_size
+        train_dataset, val_dataset, test_dataset=torch.utils.data.random_split( normal_dataset, [train_size, val_size, test_size])
+
+        #TODO: lists for validation sets and test sets.
+        data_object: Dict[str, torch.utils.data.Dataset] = {
+            "split_group_set": {
+                "standard_split": {
+                    "training_set": train_dataset,
+                    "validation_set": val_dataset,
+                    "test_set": test_dataset,
+                }
+            },
+        }
+
+
+        # results == {'split_covariate_data_validator': {'kolmogorov_smirnov_split_validator_method': {'training_set/test_set/feature_0': KstestResult(statistic=0.005366666666666672, pvalue=0.7785188817028788), 'training_set/validation_set/feature_0': KstestResult(statistic=0.006116666666666659, pvalue=0.6263395414856405), 'test_set/validation_set/feature_0': KstestResult(statistic=0.00660000000000005, pvalue=0.7737396658858053)}, 'kruskal_wallis_split_validator_method': {'training_set/test_set/feature_0': KruskalResult(statistic=0.06664652898325585, pvalue=0.7962835117722751), 'training_set/validation_set/feature_0': KruskalResult(statistic=0.5260585436481051, pvalue=0.4682686595849018), 'test_set/validation_set/feature_0': KruskalResult(statistic=0.14121920499019325, pvalue=0.707071953434097)}, 'mann_whitney_split_validator_method': {'training_set/test_set/feature_0': MannwhitneyuResult(statistic=600730191.0, pvalue=0.7962836482032007), 'training_set/validation_set/feature_0': MannwhitneyuResult(statistic=602051468.0, pvalue=0.4682687680109038), 'test_set/validation_set/feature_0': MannwhitneyuResult(statistic=200433932.0, pvalue=0.7070722753726866)}}}
+        results = DataDetectiveEngine().validate_from_schema(test_validation_schema, data_object)
+        method_results = results['split_covariate_data_validator']['chi_square_split_validator_method']
+        p_value = method_results['standard_split/training_set_vs_validation_set/feature_0'].pvalue
+        assert(p_value > 0.05)
+        p_value = method_results['standard_split/training_set_vs_test_set/feature_0'].pvalue
+        assert(p_value > 0.05)
+        p_value = method_results['standard_split/validation_set_vs_test_set/feature_0'].pvalue
+        assert(p_value > 0.05)
+
+    def test_negative_example(self):
+        np.random.seed(42)
+
+        test_validation_schema: Dict = {
+            "default_inclusion": False,
+            "validators": {
+                "split_covariate_data_validator": {
+                    "include": [
+                        "feature_\d+"
+                    ]
+                }
+            }
+        }
+
+        train_dataset: SyntheticCategoricalDataset = SyntheticCategoricalDataset(num_cols=1, dataset_size=10000, p=0.2)
+        val_dataset: SyntheticCategoricalDataset = SyntheticCategoricalDataset(num_cols=1, dataset_size=10000, p=0.5)
+        test_dataset: SyntheticCategoricalDataset = SyntheticCategoricalDataset(num_cols=1, dataset_size=10000, p=0.8)
+
+        #TODO: lists for validation sets and test sets.
+        data_object: Dict[str, torch.utils.data.Dataset] = {
+            "split_group_set": {
+                "standard_split": {
+                    "training_set": train_dataset,
+                    "validation_set": val_dataset,
+                    "test_set": test_dataset,
+                }
+            },
+            # "entire_set": normal_dataset
+        }
+
+        # results == {'split_covariate_data_validator': {'kolmogorov_smirnov_split_validator_method': {'training_set/test_set/feature_0': KstestResult(statistic=0.6772, pvalue=0.0), 'training_set/validation_set/feature_0': KstestResult(statistic=0.3886, pvalue=0.0), 'test_set/validation_set/feature_0': KstestResult(statistic=0.3776, pvalue=0.0)}, 'kruskal_wallis_split_validator_method': {'training_set/test_set/feature_0': KruskalResult(statistic=10622.043853937459, pvalue=0.0), 'training_set/validation_set/feature_0': KruskalResult(statistic=4177.829853541334, pvalue=0.0), 'test_set/validation_set/feature_0': KruskalResult(statistic=3899.0755428250777, pvalue=0.0)}, 'mann_whitney_split_validator_method': {'training_set/test_set/feature_0': MannwhitneyuResult(statistic=7923532.0, pvalue=0.0), 'training_set/validation_set/feature_0': MannwhitneyuResult(statistic=23611749.0, pvalue=0.0), 'test_set/validation_set/feature_0': MannwhitneyuResult(statistic=75492713.0, pvalue=0.0)}}}
+        results = DataDetectiveEngine().validate_from_schema(test_validation_schema, data_object)
+        method_results = results['split_covariate_data_validator']['chi_square_split_validator_method']
+        p_value = method_results['standard_split/training_set_vs_validation_set/feature_0'].pvalue
+        assert(p_value < 0.05)
+        p_value = method_results['standard_split/training_set_vs_test_set/feature_0'].pvalue
+        assert(p_value < 0.05)
+        p_value = method_results['standard_split/validation_set_vs_test_set/feature_0'].pvalue
+        assert(p_value < 0.05)
