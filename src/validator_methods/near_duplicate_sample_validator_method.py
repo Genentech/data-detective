fa0ad40b988ef5d7271d470f73d4fa3b4b85e5a2
Louie McConnell <mcconnl3@gene.com>
2024-04-25 09:09:20 -0700

    memory bug with triu

--- src/validator_methods/near_duplicate_sample_validator_method.py
+++ src/validator_methods/near_duplicate_sample_validator_method.py
@@ -13,12 +13,13 @@ from src.validator_methods.data_validator_method import DataValidatorMethod
 
 class NearDuplicateSampleValidatorMethod(DataValidatorMethod):
     EPS = 1e-15
+
     @staticmethod
     def datatype() -> Set[DataType]:
         """
         @return: the datatype the validators method operates on
         """
-        return { DataType.CATEGORICAL, DataType.CONTINUOUS, DataType.MULTIDIMENSIONAL }
+        return {DataType.CATEGORICAL, DataType.CONTINUOUS, DataType.MULTIDIMENSIONAL}
 
     @staticmethod
     def param_keys() -> Set[ValidatorMethodParameter]:
@@ -27,10 +28,12 @@ class NearDuplicateSampleValidatorMethod(DataValidatorMethod):
 
         @return: a list of parameters for the .validate() method.
         """
-        return { ValidatorMethodParameter.ENTIRE_SET }
+        return {ValidatorMethodParameter.ENTIRE_SET}
 
     @staticmethod
-    def get_method_kwargs(data_object: Dict[str, Dataset], validator_kwargs: Dict = None) -> Dict:
+    def get_method_kwargs(
+        data_object: Dict[str, Dataset], validator_kwargs: Dict = None
+    ) -> Dict:
         """
         Gets the arguments for each run of the validator_method, and what to store the results under.
 
@@ -40,48 +43,38 @@ class NearDuplicateSampleValidatorMethod(DataValidatorMethod):
         """
         entire_dataset: Dataset = data_object["entire_set"]
 
-        matrix = []
-
-        for idx in range(entire_dataset.__len__()):
-            sample = entire_dataset[idx]
-            matrix.append(
-                np.concatenate([k.flatten() for k in sample.values()])
-            )
-
-        matrix = np.array(matrix)
-
         kwargs_dict = {
             f"results": {
-                "data": matrix,
-                "angle_threshold": validator_kwargs.get("angle_threshold", 15)
+                "data": entire_dataset.get_matrix(column_wise=False),
+                "angle_threshold": validator_kwargs.get("angle_threshold", 15),
             }
         }
 
         return kwargs_dict
 
     @staticmethod
-    def validate( 
-        data: np.array, 
-        angle_threshold: float
-    ) -> List[Tuple[int, int]]:
+    def validate(data: np.array, angle_threshold: float) -> List[Tuple[int, int]]:
         angle_threshold = np.radians(angle_threshold)
-        cos_sim_threshold = np.cos(angle_threshold) - NearDuplicateSampleValidatorMethod.EPS
+        cos_sim_threshold = (
+            np.cos(angle_threshold) - NearDuplicateSampleValidatorMethod.EPS
+        )
 
         # Normalize vectors
         norm_data = data / np.linalg.norm(data, axis=1)[:, np.newaxis]
 
         # Calculate cosine similarities using a single matrix multiplication
-        cos_sims = norm_data @ norm_data.T
+        cos_sims = np.dot(norm_data, norm_data.T)
 
         # Set lower triangular part to zero to avoid self-comparisons and duplicates
         np.fill_diagonal(cos_sims, 0)
-        cos_sims = np.triu(cos_sims)
 
-        # Find indices where cosine similarity exceeds the threshold
         rows, cols = np.where(cos_sims >= cos_sim_threshold)
 
         # Create a list of pairs
-        # todo: add scores in too. 
-        pairs = list(zip(rows, cols))
+        # todo: add scores in too.
+        all_pairs = np.column_stack((rows, cols))
+
+        # Filter pairs where the row index is less than the column index
+        pairs = all_pairs[all_pairs[:, 0] < all_pairs[:, 1]]
 
         return pairs
